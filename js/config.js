ee0c0fdd350cb58116c8346f68add1b7145780c5
qurantests <91206259+Usamekesmo@users.noreply.github.com>
2025-08-27 20:07:23 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,11 +6,12 @@
  * رابط الويب المنشور لـ Google Apps Script.
  * !!! هام جداً: يجب استبدال هذا الرابط بالرابط الخاص بك.
  */
-export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyl6ucMyd59t-ZJJni56EuqzqYmUWo_xfyDoNT3XlGknYvamI0F2EWLes6C0fmZBJCk_A/exec";
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwnHqWBzC1lG83ICX7M3XqxSLnHFoNzW2aN4aborE3bkBNEmv4JI0QU2B1Upo5B3sDzgA/exec";
 
 /**
  * الرابط الأساسي لواجهة برمجة تطبيقات القرآن الكريم.
  */
 export const QURAN_API_BASE_URL = "https://api.alquran.cloud/v1";
+
 
 
