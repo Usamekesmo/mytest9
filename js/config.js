77cb233b30e9a7267ee22a94c28e3d217a45e529
qurantests <91206259+Usamekesmo@users.noreply.github.com>
2025-08-31 11:18:43 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,13 +4,11 @@
 
 /**
  * رابط الويب المنشور لـ Google Apps Script.
- * !!! هام جداً: يجب استبدال هذا الرابط بالرابط الخاص بك عند النشر.
+ * !!! هام جداً: يجب استبدال هذا الرابط بالرابط الجديد الذي حصلت عليه بعد إعادة النشر.
  */
-export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxdPtmaf1kUnzuQvgpvG0RoTzB5N9UXi9SlwTom67neqlrPWRYApriGKpa8Ac7nhtQl4A/exec"; // <--- استبدل هذا بالرابط الخاص بك
+export const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyOa3ql762-OomdV7RwTeRN-OvNV1V7-tHFCFAnoeDLGx1AxP3L5GapIq5HEc_TdC1MSg/exec"; // <-- ضع الرابط الجديد هنا
 
 /**
  * الرابط الأساسي لواجهة برمجة تطبيقات القرآن الكريم.
  */
 export const QURAN_API_BASE_URL = "https://api.alquran.cloud/v1";
-
-
